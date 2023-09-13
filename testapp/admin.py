2c39271bc763d5b9bab447332390d3180d565648
Assel <Assel_rakisheva@mail.ru>
2023-09-13 14:35:08 +0600

    mptt tree

--- testapp/admin.py
+++ testapp/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from .models import Category  # Импортируйте вашу модель из models.py
+from mptt.admin import MPTTModelAdmin
+
+# @admin.register(CategoryAdmin)
+# class PostAdmin(admin.ModelAdmin):
+#     list_display = ('parent', 'name')
 
 
-@admin.register(Category)
-class MyModelAdmin(admin.ModelAdmin):
-    list_display = ('parent', 'name')
+# class PostAdmin(admin.ModelAdmin):
+#     prepopulated_fields = {"slug": ("title",)}
+#
+
+# admin.site.register(Post, PostAdmin)
+
+
+class CategoryAdmin(MPTTModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Category, CategoryAdmin)
--- testapp/models.py
+++ testapp/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
 
-class Category(models.Model):
+class Category(MPTTModel):
     name = models.CharField(max_length=100)
-    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+    parent = TreeForeignKey('self', on_delete=models.PROTECT, null=True, blank=True, related_name='children',
+                            db_index=True, verbose_name='Родительская категория')
+
+        # 'self', null=True, blank=True, on_delete=models.CASCADE)
+    slug = models.SlugField()
 
     def __str__(self):
         return self.name
